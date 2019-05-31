93091c38a8704a8932847abc257a8d5451558a54
Miguel Netto <admnetto10@gmail.com>
2019-05-30 23:41:04 -0300

    Criando operacao de instanciacao

--- src/main/java/com/miguelnetto/projetomc/ProjetomcApplication.java
+++ src/main/java/com/miguelnetto/projetomc/ProjetomcApplication.java
@@ -1,13 +1,33 @@
 package com.miguelnetto.projetomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetomcApplication {
+import com.miguelnetto.projetomc.domain.Categoria;
+import com.miguelnetto.projetomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjetomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
