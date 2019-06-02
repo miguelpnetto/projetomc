b3ff7d6fce2b155a70bbe071ba7a03185adc8f42
Miguel Netto <admnetto10@gmail.com>
2019-06-02 18:56:47 -0300

    EndPoint /clientes/{id} disponivel

---
+++ src/main/java/com/miguelnetto/projetomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.miguelnetto.projetomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.miguelnetto.projetomc.domain.Cliente;
+import com.miguelnetto.projetomc.repositories.ClienteRepository;
+import com.miguelnetto.projetomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto nao encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));		
+	}
+	
+}
