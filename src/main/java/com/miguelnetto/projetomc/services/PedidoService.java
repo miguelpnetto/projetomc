2d54d35cc0f3896b93ccde3f16c18a096f8b1233
Miguel Netto <admnetto10@gmail.com>
2019-06-02 21:20:47 -0300

    EndoPoint /pedidos;{id} disponibilizados

---
+++ src/main/java/com/miguelnetto/projetomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.miguelnetto.projetomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.miguelnetto.projetomc.domain.Pedido;
+import com.miguelnetto.projetomc.repositories.PedidoRepository;
+import com.miguelnetto.projetomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto nao encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));		
+	}
+	
+}
