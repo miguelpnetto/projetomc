396389d3808fbfce2f38526d0d28c2470aa795de
Miguel Netto <admnetto10@gmail.com>
2019-06-03 22:55:42 -0300

    Listando todas as Categorias com DTO

--- src/main/java/com/miguelnetto/projetomc/services/CategoriaService.java
+++ src/main/java/com/miguelnetto/projetomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.miguelnetto.projetomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,8 +40,11 @@ public class CategoriaService {
 			repo.deleteById(id);
 		}catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possivel um categoria que possui produtos");
-		}
-		
+		}	
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 	
 }
